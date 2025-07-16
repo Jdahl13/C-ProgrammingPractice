d6266e340c885c67e94ad9415342529180c92c9e
jdahl13 <jdahl07@gmail.com>
2025-07-15 18:11:03 -0700

    Chapter 4 comment exercise, wrote comments for vars and fixed bug of double of

--- HelloWorld2/Program.cs
+++ HelloWorld2/Program.cs
@@ -1,10 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Bread is ready");
+/*Console.WriteLine("Bread is ready");
 Console.WriteLine("Who is the bread for?");
 string name;
-name = Console.ReadLine();
+name = Console.ReadLine();// this warning is expected and a none issue
 Console.WriteLine("Noted: " + name + " got bread");
+*/
+//Practice exercise chapter 4 on comments
+Console.WriteLine("What kind of thing are we talking about?");
+string a = Console.ReadLine(); //stores input in string a
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?"); 
+string b = Console.ReadLine(); // stores input of string b
+string c = "of Doom"; // stores input of string c
+string d = "3000"; /* stores input of string d */
+Console.WriteLine("The " + b + " " + a + " "  + c + " " + d + "!"); 
 
 
 
